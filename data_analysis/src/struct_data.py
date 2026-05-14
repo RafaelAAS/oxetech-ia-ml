2d10818e0a36a7ddab196710dbac874d5463ac2a
Rafael Anacleto <rafael.anacleto@hotmail.com>
2026-05-14 14:23:21 -0300

    feat: implement the requirements 2

--- data_analysis/src/struct_data.py
+++ data_analysis/src/struct_data.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 
 from ucimlrepo import fetch_ucirepo
@@ -5,7 +6,7 @@ from ucimlrepo import fetch_ucirepo
 
 class DataBase:
 
-    def struct_database(self):
+    def struct_database(self) -> object:
         breast_cancer_wisconsin_diagnostic = fetch_ucirepo(id=17)
         
         feature = breast_cancer_wisconsin_diagnostic.data.features
@@ -16,8 +17,26 @@ class DataBase:
         data['Diagnosis'] = data['Diagnosis'].replace('B', 0)
         print(data.shape)
 
+        return data
+
+    def biological_asymmetry(self, data: object):
+        mean = data['area1'].mean()
+        median = data['area1'].median()
+        standard_deviation = data['area1'].std()
+        symmetry = data['area1'].skew()
+
+        print(f"Mean: {mean} | Median: {median} | SD: {standard_deviation} | Symmetry: {symmetry}")
+
+        data['area1'].plot.density()
+        plt.title("Density Distribution - Cell Nucleus Area")
+        plt.xlabel("Area (area1)")
+        plt.ylabel("Density")
+        plt.show()
+
 
 if __name__ == "__main__":
 
     play = DataBase()
-    play.struct_database()
+    data = play.struct_database()
+
+    play.biological_asymmetry(data)
