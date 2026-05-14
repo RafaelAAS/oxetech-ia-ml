39c2f580be4690479459616e383ba2fa93acbbd2
Rafael Anacleto <rafael.anacleto@hotmail.com>
2026-05-13 23:44:48 -0300

    feat: implement the requirement 1

--- data_analysis/src/struct_data.py
+++ data_analysis/src/struct_data.py
@@ -2,4 +2,22 @@ import pandas as pd
 
 from ucimlrepo import fetch_ucirepo
 
-breast_cancer_wisconsin_diagnostic = fetch_ucirepo(id=17)
+
+class DataBase:
+
+    def struct_database(self):
+        breast_cancer_wisconsin_diagnostic = fetch_ucirepo(id=17)
+        
+        feature = breast_cancer_wisconsin_diagnostic.data.features
+        target = breast_cancer_wisconsin_diagnostic.data.targets
+
+        data = pd.concat([feature, target], axis=1)
+        data['Diagnosis'] = data['Diagnosis'].replace('M', 1)
+        data['Diagnosis'] = data['Diagnosis'].replace('B', 0)
+        print(data.shape)
+
+
+if __name__ == "__main__":
+
+    play = DataBase()
+    play.struct_database()
